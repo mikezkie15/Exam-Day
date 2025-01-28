abf5fcec49ca205fa194ea6a402028d85e14a8ee
Mike <sordillamike1@gmail.com>
2025-01-28 20:11:16 +0800

    60 problem add

---
+++ pythonSolving/60.problem.py
@@ -0,0 +1,11 @@
+# Get 5 author Name with their Book Name
+# Display last author with its Book Name
+
+library = {}
+
+for i in range(5):
+    author_name = input("Enter author name: ")
+    book_name = input("Enter Book name: ")
+    library[author_name] = book_name
+print("First Author is " + str(list(library.keys())[0]))
+print("Bookname is " + str(list(library.values())[0]))
